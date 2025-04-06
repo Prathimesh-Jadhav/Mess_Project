dc77fdeb8181160552a2198bab7144dea19ae462
Prathimesh-123 <jadhavprathimesh0710@gmail.com>
2025-04-06 17:24:18 +0530

    small changes

--- backend/routes/paymentsRoute.js
+++ backend/routes/paymentsRoute.js
@@ -3,16 +3,7 @@ const router = express.Router();
 const { getPaymentDetails,handleAmountPaid,getPaymentHistory,getDuePayments } = require("../controllers/paymentsController");
 const Payment = require("../models/paymentsModel");
 
-router.post('/paymentDetails',getPaymentDetails)
-// router.post("/", async (req, res) => {
-//     try {
-//         const payment = new Payment(req.body);
-//         await payment.save();
-//         res.status(201).json(payment);
-//     } catch (error) {
-//         res.status(400).json({ error: error.message });
-//     }
-// });
+router.post('/paymentDetails',getPaymentDetail);
 router.put('/amountPaid',handleAmountPaid)
 router.post('/getPaymentHistory',getPaymentHistory)
 router.post('/getDuePayments',getDuePayments)
